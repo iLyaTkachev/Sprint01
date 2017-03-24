d9521853f3db57b46446a88472e1875950a6d911
iLya Tkachev <ilya@iLyas-Mac.local>
2017-03-24 20:41:46 +0300

    No auto layout yet!

---
+++ Sprint01ARC/Sprint01ARC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sprint01ARC
+//
+//  Created by iLya Tkachev on 3/24/17.
+//  Copyright © 2017 iLya Tkachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+@end
+
